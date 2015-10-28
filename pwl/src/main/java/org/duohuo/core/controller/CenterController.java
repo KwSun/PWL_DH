761037447466335bdedba97d071be85d6981e5b0
KwSun <1023519351@qq.com>
2015-10-28 12:45:08 +0800

    add some .xml files and finish the basic configure

---
+++ pwl/src/main/java/org/duohuo/core/controller/CenterController.java
@@ -0,0 +1,14 @@
+package org.duohuo.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+ * @author Kw
+ *
+ * Date: 2015年10月28日
+ */
+@Controller
+public class CenterController {
+
+}
